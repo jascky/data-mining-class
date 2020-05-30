b6c141813947a98436c58b522e71c7b370130427
Antonio Salgado <jsalgado@arkusnexus.com>
2020-05-29 21:04:40 -0700

    added unit2 notes

--- unidad-2/practicas/practica-1.R
+++ unidad-2/practicas/practica-1.R
@@ -5,23 +5,19 @@ getwd()
 # Importing the dataset
 dataset <- read.csv('SAT-SCORES.csv')
 
-# Splitting the dataset into the Training set and Test set
-# Install.packages('caTools')
+
 library(caTools)
 set.seed(123)
 split <- sample.split(dataset$Gpa, SplitRatio = 2/3)
 training_set <- subset(dataset, split == TRUE)
 test_set <- subset(dataset, split == FALSE)
 
-# Fitting Simple Linear Regression to the Training set
 regressor = lm(formula = Gpa ~ Sat,
                data = dataset)
 summary(regressor)
 
-# Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
-# Visualising the Training set results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x=training_set$Sat, y=training_set$Gpa),
@@ -32,7 +28,6 @@ ggplot() +
   xlab('SAT') +
   ylab('GPA')
 
-# Visualising the Test set results
 ggplot() +
   geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
              color = 'red') +
